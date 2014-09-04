ab9747c43aa9485671217560b6039a114d97a17a
Tom Rishworth <tom@webscope.co.nz>
2014-09-05 08:55:53 +1200

    Init commit

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		watch: {
+			sass: {
+				files: ['sass/**/*.{scss,sass}','sass/_partials/**/*.{scss,sass}'],
+				tasks: ['sass:dist']
+			},
+		},
+		sass: {
+			options: {
+				sourceComments: 'map',
+				outputStyle: 'compressed'
+			},
+			dist: {
+			  options: {
+          includePaths: require('node-bourbon').includePaths
+        },
+				files: {
+					'stylesheets/style.css': 'sass/style.scss'
+				}
+			}
+		},
+		uglify: {
+      dev: {
+        options: {
+          mangle: false,
+          compress: false,
+          beautify: true
+        },
+        files: [{
+          expand: true,
+          flatten: true,
+          cwd: 'js',
+          dest: 'js',
+          src: ['**/*.js', '!**/*.min.js'],
+          rename: function(dest, src) {
+            var folder = src.substring(0, src.lastIndexOf('/'));
+            var filename = src.substring(src.lastIndexOf('/'), src.length);
+            filename = filename.substring(0, filename.lastIndexOf('.'));
+            return dest + '/' + folder + filename + '.min.js';
+          }
+        }]
+      },
+      dist: {
+        options: {
+          mangle: true,
+          compress: true
+        },
+        files: [{
+          expand: true,
+          flatten: true,
+          cwd: 'js',
+          dest: 'js',
+          src: ['**/*.js', '!**/*.min.js'],
+          rename: function(dest, src) {
+            var folder = src.substring(0, src.lastIndexOf('/'));
+            var filename = src.substring(src.lastIndexOf('/'), src.length);
+            filename = filename.substring(0, filename.lastIndexOf('.'));
+            return dest + '/' + folder + filename + '.min.js';
+          }
+        }]
+      }
+    }
+	});
+	grunt.registerTask('default', ['sass:dist', 'watch']);
+	grunt.registerTask('build', ['uglify:dev']);
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-preen');
+};
--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-WS-Bootstrap
+ws_bootstrap
 ============
 
-Drupal bootstrap subtheme
+A Drupal Subtheme of Bootstrap Theme with Sass
+
+
+Getting Started
+===============
+
+`npm install`
+
+`grunt` to compile scss and watch
+
+`grunt build` to compile js
+
+
