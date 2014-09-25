0040a3206ec216001674e4e756fd25f206c2b640
Tom Rishworth <tom@webscope.co.nz>
2014-09-26 11:31:39 +1200

    Add concat & uglify for javascript

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,15 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		watch: {
+      // options: {
+      //   livereload: true
+      // },
 			sass: {
 				files: ['sass/**/*.{scss,sass}','sass/_partials/**/*.{scss,sass}'],
-				tasks: ['sass:dist']
+				tasks: ['sass:dist'],
+        options: {
+          livereload: true
+        }
 			},
 		},
 		sass: {
@@ -21,51 +27,32 @@ module.exports = function(grunt) {
 				}
 			}
 		},
-		uglify: {
-      dev: {
-        options: {
-          mangle: false,
-          compress: false,
-          beautify: true
-        },
-        files: [{
-          expand: true,
-          flatten: true,
-          cwd: 'js',
-          dest: 'js',
-          src: ['**/*.js', '!**/*.min.js'],
-          rename: function(dest, src) {
-            var folder = src.substring(0, src.lastIndexOf('/'));
-            var filename = src.substring(src.lastIndexOf('/'), src.length);
-            filename = filename.substring(0, filename.lastIndexOf('.'));
-            return dest + '/' + folder + filename + '.min.js';
-          }
-        }]
+    concat: {
+      option: {
+        separator: ';'
       },
+      dist: {
+        src: ['javascripts/**/*.js'],
+        dest: 'javascripts/<%= pkg.name %>.js'
+      }
+    },
+		uglify: {
       dist: {
         options: {
           mangle: true,
-          compress: true
+          compress: true,
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
         },
-        files: [{
-          expand: true,
-          flatten: true,
-          cwd: 'js',
-          dest: 'js',
-          src: ['**/*.js', '!**/*.min.js'],
-          rename: function(dest, src) {
-            var folder = src.substring(0, src.lastIndexOf('/'));
-            var filename = src.substring(src.lastIndexOf('/'), src.length);
-            filename = filename.substring(0, filename.lastIndexOf('.'));
-            return dest + '/' + folder + filename + '.min.js';
-          }
-        }]
+        files: {
+          'javascripts/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
       }
     }
 	});
 	grunt.registerTask('default', ['sass:dist', 'watch']);
-	grunt.registerTask('build', ['uglify:dev']);
+	grunt.registerTask('deploy', ['concat:dist', 'uglify:dist']);
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 };
